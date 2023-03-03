9111c7896f3a91036921963767563347fcd7f524
Sai Nikhil Damacherla <sdamache@asu.edu>
2023-03-03 03:03:07 +0000

    Added rollbar code

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -29,6 +29,10 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 # from opentelemetry.sdk.trace.export import ConsoleSpanExporter, SimpleSpanProcessor
 
+## Rollbar
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
 
 # ## CloudWatch Logs
 # import watchtower
@@ -57,17 +61,35 @@ provider.add_span_processor(processor)
 # # Display the spans from the local docker env
 # console_processor = SimpleSpanProcessor(ConsoleSpanExporter())
 # provider.add_span_processor(console_processor)
-
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
-## AWS X-Ray
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+
+# ## AWS X-Ray
+# xray_url = os.getenv("AWS_XRAY_URL")
+# xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 
 #Application code for flask
 app = Flask(__name__)
+
+## Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 
 ## HoneyComb -------
@@ -75,12 +97,13 @@ app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
-## AWS X-Ray
-XRayMiddleware(app, xray_recorder)
+# ## AWS X-Ray
+# XRayMiddleware(app, xray_recorder)
 
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
+
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
@@ -96,6 +119,12 @@ cors = CORS(
 #     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
 #     return response
 
+
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
+  
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
   user_handle  = 'andrewbrown'
--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -3,7 +3,7 @@ from aws_xray_sdk.core import xray_recorder
 
 class UserActivities:
   def run(user_handle):
-    segment = xray_recorder.begin_segment('user_activities')
+    # segment = xray_recorder.begin_segment('user_activities')
     model = {
       'errors': None,
       'data': None
@@ -25,12 +25,12 @@ class UserActivities:
       }]
       model['data'] = results
       
-    # AWS X-ray
-    subsegment = xray_recorder.begin_subsegment('mock-data')
+    # # AWS X-ray
+    # subsegment = xray_recorder.begin_subsegment('mock-data')
     
-    dict = {
-      "now": now.isoformat(),
-      "results_length": len(results)
-    }
-    subsegment.put_metadata('now', dict, 'namespace')
+    # dict = {
+    #   "now": now.isoformat(),
+    #   "results_length": len(results)
+    # }
+    # subsegment.put_metadata('now', dict, 'namespace')
     return model
