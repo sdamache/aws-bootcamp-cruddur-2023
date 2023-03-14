5d83ae80e6d78f86929c6079ee2e5c77a0253c94
Sai Nikhil Damacherla <sdamache@asu.edu>
2023-03-14 04:25:31 +0000

    Resolved merge conflict for home_activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,8 +1,8 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-tracer = trace.get_tracer("home.activities")
 
+tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run():
    #Logger.info('Hello Cloudwatch! from  /api/activities/home')  
@@ -53,3 +53,6 @@ class HomeActivities:
     span.set_attribute("app.result_length", len(results))
     return results
     
+
+      span.set_attribute("app.result_length", len(results))
+      return results
