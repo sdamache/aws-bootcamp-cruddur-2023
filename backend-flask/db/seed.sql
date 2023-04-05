42d9de6dd76921254bfaaf8ff355bf702cf7e336
Sai Nikhil Damacherla <sdamache@asu.edu>
2023-04-05 03:19:11 +0000

    Added bash scripts for fetching and seeding dynamodb

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,13 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email,handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrew@exampro.co','andrewbrown' ,'MOCK'),
-  ('Andrew Bayko', 'bayko@exampro.co','bayko' ,'MOCK');
+  ('Sdamache', 'sdamache@asu.edu','sdamache' ,'MOCK'),
+  ('Sai Nikhl', 'nikhild.sai@gmail.com','nikhildsai' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'nikhildsai' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
