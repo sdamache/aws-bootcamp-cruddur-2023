ee31d779ae397cd5eaed513da0dffcc8c6a94aa2
Sai Nikhil Damacherla <sdamache@asu.edu>
2023-04-27 00:20:53 -0400

    Modified front end for edit profile button

--- frontend-react-js/src/pages/UserFeedPage.js
+++ frontend-react-js/src/pages/UserFeedPage.js
@@ -6,9 +6,10 @@ import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
+import EditProfileButton from '../components/EditProfileButton';
+import {checkAuth,getAccessToken} from '../lib/CheckAuth';
 
-// [TODO] Authenication
-import Cookies from 'js-cookie'
+
 
 export default function UserFeedPage() {
   const [activities, setActivities] = React.useState([]);
@@ -17,33 +18,27 @@ export default function UserFeedPage() {
   const dataFetchedRef = React.useRef(false);
 
   const params = useParams();
-  const title = `@${params.handle}`;
 
   const loadData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${title}`
+      await getAccessToken()
+      const access_token = localStorage.getItem('access_token')
       const res = await fetch(backend_url, {
+        headers: {
+          Authorization: `Bearer ${access_token}`
+        },
         method: "GET"
       });
       let resJson = await res.json();
       if (res.status === 200) {
-        setActivities(resJson)
+        setActivities(resJson.profile)
+        setActivities(resJson.activities)
       } else {
         console.log(res)
       }
     } catch (err) {
       console.log(err);
-    }
-  };
-
-  const checkAuth = async () => {
-    console.log('checkAuth')
-    // [TODO] Authenication
-    if (Cookies.get('user.logged_in')) {
-      setUser({
-        display_name: Cookies.get('user.name'),
-        handle: Cookies.get('user.username')
-      })
     }
   };
 
@@ -53,7 +48,7 @@ export default function UserFeedPage() {
     dataFetchedRef.current = true;
 
     loadData();
-    checkAuth();
+    checkAuth(setUser);
   }, [])
 
   return (
@@ -61,7 +56,14 @@ export default function UserFeedPage() {
       <DesktopNavigation user={user} active={'profile'} setPopped={setPopped} />
       <div className='content'>
         <ActivityForm popped={popped} setActivities={setActivities} />
-        <ActivityFeed title={title} activities={activities} />
+
+        <div className='activity_feed'>
+          <div className='activity_feed_heading'>
+            <div className='title'>{profile.display_name}</div>
+            <div class="cruds_count">{profile.cruds_count} Cruds</div>
+          </div>
+          <ActivityFeed activities={activities} />
+        </div>
       </div>
       <DesktopSidebar user={user} />
     </article>
