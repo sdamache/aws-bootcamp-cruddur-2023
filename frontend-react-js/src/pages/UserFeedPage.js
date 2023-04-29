1aad831871e5c00f04ee0c9b390d72ac9790e0de
Sai Nikhil Damacherla <sdamache@asu.edu>
2023-04-29 19:58:27 -0400

    Modified user feed page js for separating profile button

--- frontend-react-js/src/pages/UserFeedPage.js
+++ frontend-react-js/src/pages/UserFeedPage.js
@@ -6,14 +6,17 @@ import DesktopNavigation  from '../components/DesktopNavigation';
 import DesktopSidebar     from '../components/DesktopSidebar';
 import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
-import EditProfileButton from '../components/EditProfileButton';
 import {checkAuth,getAccessToken} from '../lib/CheckAuth';
+import ProfileHeading from '../components/ProfileHeading';
 
 
 
 export default function UserFeedPage() {
   const [activities, setActivities] = React.useState([]);
   const [popped, setPopped] = React.useState([]);
+  const [profile, setProfile] = React.useState([]); 
+  const [poppedProfile, setPoppedProfile] = React.useState([]);
+
   const [user, setUser] = React.useState(null);
   const dataFetchedRef = React.useRef(false);
 
@@ -21,7 +24,7 @@ export default function UserFeedPage() {
 
   const loadData = async () => {
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${title}`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/@${params.handle}`
       await getAccessToken()
       const access_token = localStorage.getItem('access_token')
       const res = await fetch(backend_url, {
@@ -30,9 +33,11 @@ export default function UserFeedPage() {
         },
         method: "GET"
       });
+      console.log("Display_name")
+      console.log(profile.cruds_count);
       let resJson = await res.json();
       if (res.status === 200) {
-        setActivities(resJson.profile)
+        setProfile(resJson.profile)
         setActivities(resJson.activities)
       } else {
         console.log(res)
@@ -58,10 +63,7 @@ export default function UserFeedPage() {
         <ActivityForm popped={popped} setActivities={setActivities} />
 
         <div className='activity_feed'>
-          <div className='activity_feed_heading'>
-            <div className='title'>{profile.display_name}</div>
-            <div class="cruds_count">{profile.cruds_count} Cruds</div>
-          </div>
+          <ProfileHeading setPopped={setPoppedProfile} profile={profile} />
           <ActivityFeed activities={activities} />
         </div>
       </div>
