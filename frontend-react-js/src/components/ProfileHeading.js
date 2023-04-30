3cb762c07b45e75cb686f7dda7e45c9252f25ca8
Sai Nikhil Damacherla <sdamache@asu.edu>
2023-04-30 18:17:07 -0400

    Added front end js code to show bio from the database

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -26,6 +26,7 @@ export default function ProfileHeading(props) {
           </div>
         <EditProfileButton setPopped={props.setPopped} />
     </div>
+    <div class="bio">{props.profile.bio}</div>
   
   </div>
   
