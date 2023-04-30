01cf94fb0cc92bffa0157c6fb71de4b5692cbb3a
Sai Nikhil Damacherla <sdamache@asu.edu>
2023-04-30 18:10:04 -0400

    Added profile form to user feed to update the profile

--- frontend-react-js/src/pages/UserFeedPage.js
+++ frontend-react-js/src/pages/UserFeedPage.js
@@ -8,6 +8,7 @@ import ActivityFeed from '../components/ActivityFeed';
 import ActivityForm from '../components/ActivityForm';
 import {checkAuth,getAccessToken} from '../lib/CheckAuth';
 import ProfileHeading from '../components/ProfileHeading';
+import ProfileForm from '../components/ProfileForm';
 
 
 
@@ -33,8 +34,6 @@ export default function UserFeedPage() {
         },
         method: "GET"
       });
-      console.log("Display_name")
-      console.log(profile.cruds_count);
       let resJson = await res.json();
       if (res.status === 200) {
         setProfile(resJson.profile)
@@ -61,7 +60,11 @@ export default function UserFeedPage() {
       <DesktopNavigation user={user} active={'profile'} setPopped={setPopped} />
       <div className='content'>
         <ActivityForm popped={popped} setActivities={setActivities} />
-
+        <ProfileForm
+          profile={profile}
+          popped={poppedProfile}
+          setPopped={setPoppedProfile}
+        />
         <div className='activity_feed'>
           <ProfileHeading setPopped={setPoppedProfile} profile={profile} />
           <ActivityFeed activities={activities} />
