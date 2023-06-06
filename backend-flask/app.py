8b4d4c34fed3018cb7609ad83f4934ace6860b46
Sai Nikhil Damacherla <sdamache@asu.edu>
2023-06-06 05:50:31 +0000

    Modified roll bar initialization route

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -85,7 +85,7 @@ cognito_token_verification = CognitoTokenVerification(
 
 ## Rollbar
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
+@app.before_request
 def init_rollbar():
     """init rollbar module"""
     rollbar.init(
