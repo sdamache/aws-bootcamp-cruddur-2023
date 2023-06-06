814e4329f865aef0aec0d6f5f047ec49cf245930
Sai Nikhil Damacherla <sdamache@asu.edu>
2023-06-06 00:52:51 -0400

    Modified Profile Form to directly pass pre-signed url

--- frontend-react-js/src/components/ProfileForm.js
+++ frontend-react-js/src/components/ProfileForm.js
@@ -14,9 +14,16 @@ export default function ProfileForm(props) {
     setDisplayName(props.profile.display_name);
   }, [props.profile])
   
-  const s3uploadkey = async (event) => {
+  const s3uploadkey = async (event)=> {
+    console.log('event',event)
+    // const file = event.target.files[0]
+    // const filename = file.name
+    // const size = file.size
+    // const type = file.type
+    // const preview_image_url = URL.createObjectURL(file)
+    // console.log(filename,size,type)
     try {
-      console.log("s3upload")
+      console.log('s3uploadkey')
       const backend_url = "https://lje9l2bhjg.execute-api.us-east-1.amazonaws.com/avatars/key_upload"
       await getAccessToken()
       const access_token = localStorage.getItem("access_token")
@@ -48,14 +55,19 @@ export default function ProfileForm(props) {
     console.log(filename,size,type)
     try {
       console.log('s3upload')
-      const res = await fetch(presignedurl, {
+      const backend_url = "https://s3.amazonaws.com/cruddur-uploaded-avatars.lol/mock.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVS36AKMVOLHL4P7G%2F20230606%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230606T044125Z&X-Amz-Expires=300&X-Amz-SignedHeaders=host&X-Amz-Signature=0077e4718f65e4dc3ce0e597f187aa7eb801bcb0859fc1cc59604dc595555fdb"
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token")
+      const res = await fetch(backend_url, {
         method: "PUT",
         body: file,
         headers: {
           'Content-Type': type
-      }})
+        }
+      });
+      let data = await res.json();
       if (res.status === 200) {
-        
+        console.log('pre-signed url', data)
       } else {
         console.log(res)
       }
@@ -123,9 +135,13 @@ export default function ProfileForm(props) {
             </div>
           </div>
           <div className="popup_content">
-            
-            <input type="file" name="avatarupload" onChange={s3upload} />
-  
+            <div className="upload" onClick={s3uploadkey}>
+              Upload Avatar
+            </div>
+          <input type="file" name="avatarupload" onChange={s3upload} />
+            {/* <div className="upload" onClick={s3upload}>
+                Upload Avatar for real
+              </div> */}
               <div className="field display_name">
                 <label>Display Name</label>
                 <input
