c87d02f42306a371a3999f3ea7dfd6791c39aace
Sai Nikhil Damacherla <sdamache@asu.edu>
2023-06-17 21:11:37 +0000

    Modified the background image variables

--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -1,7 +1,7 @@
 import './ProfileAvatar.css';
 
 export default function ProfileAvatar(props) {
-  const backgroundImage = `url("https://assets.cruddur.lol/avatars/${props.user.cognito_user_uuid}.jpg")`;
+  const backgroundImage = `url("https://assets.cruddur.lol/avatars/${props.id}.jpg")`;
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
